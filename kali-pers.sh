ff4fc295836171d2ac250f226d07ec2f9421bf6d
aakyol <aydinaakyol@gmail.com>
2016-06-04 17:07:45 +0300

    Bash File Initial Upload
    Initial upload of the original batch file.

---
+++ kali-pers.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#copyright Aydın Akyol 2016
+echo "Enter the USB sdX string to make a partition for persistence: "
+read USBport
+mkfs.ext3 -L persistence /dev/$USBport
+e2label /dev/$USBport persistence
+mkdir -p /mnt/my_usb
+mount /dev/$USBport /mnt/my_usb
+echo "/ union" > /mnt/my_usb/persistence.conf
+umount /dev/$USBport
+echo "Persistence process is completed on the bash side." 
+echo "Please check if the persistence is consistent, by simply creating a file on desktop and restart the OS."
